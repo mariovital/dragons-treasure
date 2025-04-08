de22d4ac320642799fe4932f7f91dad7a001f87c
vitalagency <93239157+vitalagency@users.noreply.github.com>
2025-04-08 11:54:59 -0600

    index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+
+import express from 'express'
+import cors from 'cors'
+import multer from 'multer'
+
+const app = epress()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(multer.array())
+app.use(cors())
+
+
+app.get('/', (req,res) => {
+    res.send('Hola desde Dragon Treasure')
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`)
+})
