8c4fbb0adc3760473ca945f82576c7b89ba8d420
vitalagency <93239157+vitalagency@users.noreply.github.com>
2025-04-08 16:17:36 -0600

    added First Demo Route

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from 'express'
 import cors from 'cors'
 import multer from 'multer'
 
+
+import {router as usuario} from './routes/usuario.js'
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -10,11 +13,20 @@ app.use(express.json())
 app.use(multer().array())
 app.use(cors())
 
+// Rutas
+app.use('/usuario', usuario)
 
 app.get('/', (req,res) => {
-    res.send('Hola desde Dragon Treasure')
+    res.send(`API Dragon's Treasure`)
 })
 
+// Manejo de errores
+app.use((err, req, res, next) => {
+    console.error(err,stack)
+    res.status(500).json({ error: 'Error interno del servidor.'})
+})
+
+// Iniciar Servidor.
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}`)
 })
