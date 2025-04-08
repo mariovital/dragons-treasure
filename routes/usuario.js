ff3c4926b7650c9414d5f50eefee0e01f0ee01e8
vitalagency <93239157+vitalagency@users.noreply.github.com>
2025-04-08 16:07:52 -0600

    Middleware usuario.js

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,7 +3,8 @@ import express from 'express'
 const router = express.Router()
 
 import { getUser} from '../controllers/users.controller.js'
+import { middleware } from '../middleware/jwt.middleware.js'
 
-router.post('/usuario',getUser)
+router.post('/usuario', middleware ,getUser)
 
 export { router } 
