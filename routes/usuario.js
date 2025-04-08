9aebc0bcef6872c04154351431d208524c346356
vitalagency <93239157+vitalagency@users.noreply.github.com>
2025-04-08 16:53:04 -0600

    Usuario debug

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,8 +3,13 @@ import express from 'express'
 const router = express.Router()
 
 import { getUser} from '../controllers/usuario.controller.js'
-import { middleware } from '../middleware/jwt.middleware.js'
 
-router.post('/usuario', middleware ,getUser)
+
+router.get('/:id', getUser)
+
+// Add this to routes/usuario.js
+router.get('/test', (req, res) => {
+    res.json({ message: 'API is working' });
+  });
 
 export { router } 
